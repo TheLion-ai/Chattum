1c135e30f4d763f90b4724de94e218b53c2a9006
a-huli <klaudel.b@gmail.com>
2023-05-22 15:33:43 +0200

    Add test for nonexistent bots

--- backend/tests/test_prompts.py
+++ backend/tests/test_prompts.py
@@ -1,7 +1,9 @@
 """Tests for the backend application."""
 import unittest
+from itertools import permutations
 
 from app import app
+from bson import ObjectId
 from fastapi.testclient import TestClient
 
 # Remove the sorting of test methods
@@ -32,6 +34,12 @@ class TestPrompts(unittest.TestCase):
         response = self.test_client.get(f"/bots/{self.bot_id}/prompt")
         assert response.status_code == 200
         assert response.json() == {"prompt": "Hello World!"}
+
+    def test_get_prompt_not_found(self) -> None:
+        """Test the get prompt endpoint with a bot that does not exist."""
+        fake_id = ObjectId("123456789012345678901234")
+        response = self.test_client.get(f"/bots/{fake_id}/prompt")
+        assert response.status_code == 404
 
     @classmethod
     def tearDownClass(cls) -> None:
